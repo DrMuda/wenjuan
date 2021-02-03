4cf1e538b8225bb3b0965b615c9c980da3048c99
木大博士a <1487537353@qq.com>
2021-02-03 14:32:55 +0800

    更改配置

--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,12 @@
 module.exports = {
     devServer: {
+        disableHostCheck: true,
         proxy: {
             '^/api': {
                 target: 'http://192.168.20.33:8085',
                 changeOrigin: true,
                 ws: true
             }
-        }
+        },
     }
 }
