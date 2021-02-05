28318102e45e074e080b4e62f8f9f37fedd97119
木大博士a <1487537353@qq.com>
2021-02-05 17:28:45 +0800

    修改接口

--- vue.config.js
+++ vue.config.js
@@ -3,16 +3,16 @@ module.exports = {
     devServer: {
         disableHostCheck: true,
         proxy: {
+        //     '^/api': {
+        //         target: 'http://120.27.3.70:31800',
+        //         changeOrigin: true,
+        //         ws: true
+        //     },
             '^/api': {
-                target: 'http://120.27.3.70:31800',
+                target: "http://192.168.20.26:8085",
                 changeOrigin: true,
                 ws: true
-            },
-            // '^/api': {
-            //     target: "http://192.168.20.26:8085",
-            //     changeOrigin: true,
-            //     ws: true
-            // }
+            }
         },
     }
 }
